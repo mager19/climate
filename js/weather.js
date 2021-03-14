5ecdbaa1724cf98862ebcc80ba1a57a22dfdb1e3
mager19 <mager19@gmail.com>
2021-03-14 15:02:07 -0500

    added https to the request

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@
 //a4a63d987a7e8af14df9da323969afcc
 
 async function weather(lat, lon) {
-	let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=700367c59f833d4814d7f025d13afbfc&units=metric`;
+	let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=700367c59f833d4814d7f025d13afbfc&units=metric`;
 
 	const getWeather = await fetch(url);
 	const response = await getWeather.json();
